7dcec2ffe5a5c1cc12fe4ad595889c418971f452
jdn <duane.northcutt@gmail.com>
2018-01-18 16:06:32 -0800

    Updated.  Still WIP and non-functional.

--- teslaWatch.py
+++ teslaWatch.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
- 
+#!/usr/bin/env python
+
 ################################################################################
 #
 # Script to watch for Tesla state changes
@@ -8,276 +8,209 @@
 #  manage the fences and this will issue notifications to the mobile device.
 #
 ################################################################################
- 
-import sys
-import time
+
+import argparse
 import json
-import random
-import LatLon
-import dateutil
+import os
+import sys
 import teslajson
-from datetime import datetime
-from optparse import OptionParser
- 
-#### FIXME make this part of the conf input file
-TESLA_CLIENT_ID = "????"
-TESLA_CLIENT_SECRET = "????"
- 
-DEF_USER = "????"
-DEF_VIN = "????"
- 
-HOME_LAT = 37.460184
-HOME_LON = -122.166203
-HOME_LOC = LatLon.LatLon(LatLon.Latitude(HOME_LAT), LatLon.Longitude(HOME_LON))
- 
-WORK_LAT = 37.3848558
-WORK_LON = -121.9947407
-WORK_LOC = LatLon.LatLon(LatLon.Latitude(WORK_LAT), LatLon.Longitude(WORK_LON))
- 
-STANFORD_LAT = 37.430598
-STANFORD_LON = -122.173109
-STANFORD_LOC = LatLon.LatLon(LatLon.Latitude(STANFORD_LAT),
-                             LatLon.Longitude(STANFORD_LON))
- 
- 
-"""
-DEG_TO_RAD = (math.pi / 180.0)
-EARTH_RADIUS = ?
- 
-# Take a pair of lat/lon points and return the distance between them
-#  in meters.
-def distance(lat1, lon1, lat2, lon2):
-    # phi = 90 - latitude
-    phi1 = (90.0 - lat1) * DEG_TO_RAD
-    phi2 = (90.0 - lat2) * DEG_TO_RAD
-          
-    # theta = longitude
-    theta1 = lon1 * DEG_TO_RAD
-    theta2 = lon2 * DEG_TO_RAD
-          
-    # compute spherical distance from spherical coordinates
-    # For two locations in spherical coordinates: (1, theta, phi) and
-    #  (1, theta', phi'), cosine( arc length ) = 
-    #    sin phi sin phi' cos(theta-theta') + cos phi cos phi'
-    # distance = rho * arc length
-    cos = (math.sin(phi1) * math.sin(phi2) * math.cos(theta1 - theta2) + 
-           math.cos(phi1) * math.cos(phi2))
-    arc = math.acos(cos)
-  
-    # multiply arc by the radius of the earth in meters to get distance
-    return arc * EARTH_RADIUS
-"""
- 
-# Base class for shapes that define geographic regions of interest.
-#### TODO consider using abc to define the abstract base class's methods
-#### For now, must provide the following methods:
-####  * getDistance(lat, lon) -- returns distance to closest edge in Km (float)
-####  * isInside(lat, lon) -- returns True if given point is inside
-class Shape(object):
-    def __init__(self, id=None):
-        if id is None:
-            self.id = str(int(random.random() * 100000))
-        else:
-            self.id = id
- 
- 
-# This object defines a circle of a given radius around a point (given my a
-#  latitude and longitude pair).
-# If no 'id' is given, a random one will be chosen.
-# Can get a distance from some coordinates to the center of this point, or to
-#  the edge of the circle.
-# Can also ask if a given location is inside the circle or not.
-# The 'radius' arg is Km (float), and 'id' is a string (or a random number is
-#  generated if none is given).
-class Circle(Shape):
-    def __init__(self, lat, lon, radius, id=None):
-        super(Circle, self).__init__(id)
-        self.location = LatLon.LatLon(LatLon.Latitude(lat),
-                                      LatLon.Longitude(lon))
-        self.radius = radius
- 
-    def _getDistance(self, lat, lon):
-        d = self.location.distance(LatLon.LatLon(LatLon.Latitude(lat),
-                                                 LatLon.Longitude(lon)))
-        return d
- 
-    def isInside(self, lat, lon):
-        d = self._getDistance(lat, lon)
-        if d < self.radius:
-            return True
-        else:
-            return False
- 
-# Take a lat/lon pair and a polygon (defined by a list of lat/lon tuples),
-#  and return True if the point is inside the polygon.
-def isInsidePolygon(lat, lon, poly):
-    n = len(poly)
-    inside = False
- 
-    p1Lat, p1Lon = poly[0]
-    for i in range(n + 1):
-        p2Lat, p2Lon = poly[i % n]
-        if lon > min(p1Lon, p2Lon):
-            if lon <= max(p1Lon, p2Lon):
-                if lat <= max(p1Lat, p2Lat):
-                    if p1Lon != p2Lon:
-                        latIntrs = (lon - p1Lon) * (p2Lat - p1Lat) / (p2Lon - p1Lon) + p1Lat
-                    if p1Lat == p2Lat or lat <= latIntrs:
-                        inside = not inside
-        p1Lat, p1Lon = p2Lat, p2Lon
-    return inside
- 
- 
-# This object defines a polygon (given by a set of latitude and longitude
-#  tuples representing verticies).
-class Polygon(Shape):
-    def __init__(self, poly, id=None):
-        super(Polygon, self).__init__(id)
-        self.poly = poly
- 
-    def isInside(self, lat, lon):
-        return isInsidePolygon(lat, lon, self.poly)
- 
- 
-# This object defines a rectangle (given by a pair of latitude and longitude
-#  pairs representing a set of diagonal corners).
-class Rectangle(Polygon):
-    def __init__(self, lat1, lon1, lat2, lon2, id=None):
-        minLat = min(lat1, lat2)
-        maxLat = max(lat1, lat2)
-        minLon = min(lon1, lon2)
-        maxLon = max(lon1, lon2)
-        poly = [(minLat, maxLon), (minLat, minLon),
-                (maxLat, minLon), (maxLat, maxLon)]
-        super(Rectangle, self).__init__(poly, id)
- 
- 
-# This object takes a circle and makes a geofence out of it.
-# ????
-# do things like delete when departs, keep track of entry and exit times,
-#  keep track of last point
-class Fence(Shape):
-    def __init__(self, shape, onEnter=None, onExit=None):
-        self.shape = shape
-        self.onEnter = onEnter
-        self.onExit = onExit
-        self.lat = self.lon = None
-        self.lastUpdated = None
-        self.inside = None
-        self.enterTime = self.exitTime = None
- 
-    def update(self, lat, lon, time):
-        self.lat = lat
-        self.lon = lon
-        self.time = time
-        now = datetime.now()
-        self.lastUpdated = now
-        inside = self.shape.isInside(lat, lon)
-        if self.inside is None:
-            self.inside = inside
-        else:
-            if self.inside != inside:
-                # got a state transition
-                if inside:
-                    # wasn't inside, and now it is
-                    self.enterTime = now
-                    callback = self.onEnter
-                else:
-                    # was inside, and now it isn't
-                    self.exitTime = now
-                    callback = self.onExit
-                # ????
- 
- 
+import yaml
+
+#### TODO:
+####  * Geofence -- notify when arriving/departing locations (e.g., home)
+####  * notify on state transitions (was moving and now stopped, vice versa)
+####  * monitor temp and turn on a/c if too hot
+####  * look at weather and make sure roof is closed if raining
+####  * update more frequently when moving
+####  * notify if within X distance of current location
+####  * rewrite all of this
+####  * use multiple instances or multiprocessing to handle multiple cars
+####  * focus on state transitions: states=[parked, moving], events=[parked->moving, moving->parked]
+####  * different polling intervals for different states -- e.g., parked@home, nighttime, moving, etc.
+####  * use different packages to send SMS notifications
+####    - textbelt
+####    - google hangouts: "hangups" package pythone3
+####  * make notification backend be pluggable
+####  * rewrite to be Python3
+####  * fix up the cmd-line interface
+####  * build mapping front-end to define/display geofences
+####  * make basic features work without geofencing
+####  * define config file format
+
+#### FIXME keep textbelt key secret too
+
+'''
+DESIGN NOTES:
+* Array of cars objects
+* Region Object encapsulates
+  - coordinates that define a closed region
+    * e.g., center and radius, polygon vertices
+* Car Object encapsulates
+  - state of car
+    * States: Parked (Home/Work/Other), Moving, Stopped, Unknown
+    * recent history of states
+  - secrets/keys associated with car
+  - event handler callback
+  - Region objects
+    * defined Home and Work regions
+    * other regions of interest for the car
+* Config file:
+  - YAML
+  - secret/protected file
+  - read/updated by app
+  - one or more cars
+  - contents
+    * user name
+    * VIN
+    * Tesla API id/secret
+    * Regions (Home/Work/other)
+    * messaging info
+      - SMS: phone number, API key
+  - process:
+    * read from file
+    * match up VIN numbers with those returned from Tesla API
+    * instantiate car object for each match
+    * (optionally) whine about mismatches (on either side)
+
+--------------------
+import requests
+requests.post('https://textbelt.com/text', {
+  'phone': '5557727420',
+  'message': 'Hello world',
+  'key': 'textbelt',
+})
+
+$ curl -X POST https://textbelt.com/text \
+       --data-urlencode phone='5557727420' \
+       --data-urlencode message='Hello world' \
+       -d key=textbelt
+{"success":true,"textId":"2861516228856794","quotaRemaining":249}[jdn@jdnLinux teslawatch]$
+
+$ curl https://textbelt.com/status/2861516228856794
+{"success":true,"status":"DELIVERED"}
+'''
+
+DEF_CONFIGS_PATH = "./.teslas.yml"
+
+
+#### TODO move this to 'tesla.py'
+# Car object that encapsulates the state of a car,
+class Car(object):
+    def __init__(self, vin, config, vehicle):
+        self.vin = vin
+        self.config = config
+        self.vehicle = vehicle
+
+    def dump(self):
+        pass
+
+
 #
 # MAIN
 #
-if __name__ == '__main__':
-    usage = "Usage: {0} [-v] [-u <email>] -p <pswd> [-V <vin>]"
-    parser = OptionParser(usage)
-    parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
-                      default=False, help="print debug info")
-    parser.add_option("-u", "--user", action="store", type="string",
-                      dest="user", default=DEF_USER, help="user email address")
-    parser.add_option("-p", "--password", action="store", type="string",
-                      dest="password", default=None, help="user password")
-    parser.add_option("-V", "--VIN", action="store", type="string",
-                      dest="vin", help="Vehicle Id Number")
-    (options, args) = parser.parse_args()
- 
-    # initialize and validate inputs
-    if options.password is None:
-        sys.stderr.write("Error: must provide password\n")
+def main():
+    # Print the given message, print the usage string, and exit with an error.
+    def fatalError(msg):
+        sys.stderr.write("Error: {0}\n".format(msg))
+        sys.stderr.write("Usage: {0}\n".format(usage))
         sys.exit(1)
- 
-    user = options.user
-    password = options.password
-    vin = options.vin
- 
- 
-    # pre-execution printouts
+
+    usage = "Usage: {0} [-v] [-c <configsFile>] [-V <VIN>]"
+    ap = argparse.ArgumentParser()
+    ap.add_argument(
+        "-c", "--configsFile", action="store", type=str,
+        default=DEF_CONFIGS_PATH, help="path to file with configurations")
+    ap.add_argument(
+        "-p", "--password", action="store", type=str, help="user password")
+    ap.add_argument(
+        "-v", "--verbose", action="count", default=0, help="print debug info")
+    ap.add_argument(
+        "-V", "--VIN", action="store", type=str,
+        help="VIN of car to use (defaults to all found in config file")
+    options = ap.parse_args()
+
+    if not os.path.exists(options.configsFile):
+        fatalError("Invalid configuration file: {0}".format(options.configsFile))
+
+    #### TODO add check if configs file has proper protections
+
+    with open(options.configsFile, "r") as confsFile:
+        confs = list(yaml.load_all(confsFile))[0]
+    json.dump(confs, sys.stdout, indent=4, sort_keys=True)    #### TMP TMP TMP
+    print("")
+
+    if options.VIN:
+        carVINs = [options.VIN]
+    else:
+        carVINs = confs['CARS'].keys()
     if options.verbose:
-        sys.stdout.write("    User: {0}\n".format(user))
-        if options.vin:
-            sys.stdout.write("    VIN:  {0}\n".format(vin))
- 
-    # make the connection
+        print("CARS: {0}".format(carVINs))
+
+    user = confs.get('USER')
+    if not user:
+        user = raw_input("user: ")
+    if options.verbose:
+        print("USER: {0}".format(user))
+
+    if options.password:
+        password = options.password
+    else:
+        password = confs.get('PASSWD')
+    if not password:
+        password = raw_input("password: ")
+    if options.verbose:
+        print("PASSWD: {0}".format(password))
+
     try:
         conn = teslajson.Connection(user, password)
     except Exception as e:
-        sys.stderr.write("Error: failed to connect {0}\n".format(e))
-        sys.exit(1)
- 
-    if options.verbose:
+        fatalError("Failed to connect {0}".format(e))
+    if options.verbose > 1:
+        print("CONNECTION: {0}".format(conn))
+        print("")
+
+    if options.verbose > 1:
         print "Number of vehicles: {0}".format(len(conn.vehicles))
         n = 1
         for v in conn.vehicles:
-            print "Vehicle #{0}".format(n)
+            print "Vehicle #{0}:".format(n)
             json.dump(v, sys.stdout, indent=4, sort_keys=True)
+            print("")
             n += 1
- 
-    #### TODO deal with this when we get the next car
-    #### use the VIN to select?
-    car = conn.vehicles[0]
- 
-    # wake up the car and get the current state
-    try:
-        car.wake_up()
-        vState = car.data_request('vehicle_state')
-        if options.verbose:
-            print "Current state of the car:"
-            json.dump(vState, sys.stdout, indent=4, sort_keys=True)
-            print ""
- 
-        cState = car.data_request('climate_state')
-        if options.verbose:
-            print "Climate state of the car:"
-            json.dump(cState, sys.stdout, indent=4, sort_keys=True)
-            print ""
- 
-        dState = car.data_request('drive_state')
-        if options.verbose:
-            print "Driving state of the car:"
-            json.dump(dState, sys.stdout, indent=4, sort_keys=True)
-            print ""
-    except Exception as e:
-        sys.stderr.write("Error: failed to connect to car {0}\n".format(e))
-        sys.exit(1)
- 
-    #### TODO:
-    ####  * Geofence -- notify when arriving/departing locations (e.g., home)
-    ####  * notify on state transitions (was moving and now stopped, vice versa)
-    ####  * monitor temp and turn on a/c if too hot
-    ####  * look at weather and make sure roof is closed if raining
-    ####  * update more frequently when moving
-    ####  * notify if within X distance of current location
-    while True:
-        dState = car.data_request('drive_state')
-        if dState['shift_state'] == None:
-            time.sleep(30)
-            continue
-        if options.verbose:
-            print "Driving state of the car:"
-            json.dump(dState, sys.stdout, indent=4, sort_keys=True)
-            print ""
-        time.sleep(3)
+
+    teslaVINs = [v['vin'] for v in conn.vehicles]
+    vinList = [v for v in teslaVINs if v in carVINs]
+    if not vinList:
+        fatalError("Unable to find requested cars in Tesla API")
+
+    notFound = list(set(carVINs) - set(vinList))
+    if notFound:
+        fatalError("Cars asked for, but not found in Tesla API: {0}".format(notFound))
+
+    if options.verbose:
+        print("Watching: {0}".format(vinList))
+        notAskedFor = list(set(teslaVINs) - set(vinList))
+        if notAskedFor:
+            print("Cars Tesla API knows about, but not asked for: {0}".format(notAskedFor))
+
+    vehicles = {v['vin']: v for v in conn.vehicles if v['vin'] in vinList}
+    if options.verbose > 1:
+        print("VEHICLES:")
+        json.dump(vehicles, sys.stdout, indent=4, sort_keys=True)
+        print("")
+
+    cars = {}
+    for vin in vinList:
+        cars[vin] = Car(vin, confs['CARS'][vin], vehicles[vin])
+
+    #### TODO wake up all the cars in 'vinList'
+    for v, c in cars.items():
+        ????
+
+    #### TODO wait a bit and then go get initial data from each of the cars
+
+    #### TODO launch tracker threads for each car
+
+
+if __name__ == '__main__':
+    main()
