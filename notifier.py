95afd2d82844875a4b475a11b6c363f91cc95e0f
jdn <duane.northcutt@gmail.com>
2018-01-28 19:57:28 -0800

    Checkpoint.
      * added __init__(), __str__(), and notify() methods.
      * added minimal tests

--- notifier.py
+++ notifier.py
@@ -6,41 +6,107 @@
 ################################################################################
 '''
 
+import json
 import os
+import sys
 
 from tracker import EVENT_TYPES
 
 
 # path to notifier programs
-NOTIFIER_DIR = "."
+NOTIFIER_DIR = "./notifiers"
 
 
 class Notifier(object):
     ''' Per-car object that can encapsulates the events and (external) programs
         that re used to generate notifications for each type of event.
     '''
-    def __init__(self, eventNotifiers):
-        eventTypes = eventNotifiers.keys()
-        if not set(eventTypes).issubset(set(EVENT_TYPES)):
-            raise ValueError("Invalid event types: {0}".format(set(eventTypes) - set(EVENT_TYPES)))
-
-        for eventType, notifier in eventNotifiers.iteritems():
+    def __init__(self, notifiers):
+        self.notifiers = {}
+        for notifier, eventTypes in notifiers.iteritems():
             fPath = os.path.join(NOTIFIER_DIR, notifier)
-            if not notifier or not os.path.isfile(fPath) or not os.access(fPath, os.X_OK):
-                raise ValueError("Invalid notifier program '{0}' for event '{1}".format(fPath, eventType))
-        self.eventNotifiers = eventNotifiers
+            if not os.path.isfile(fPath) or not os.access(fPath, os.X_OK):
+                raise ValueError("Invalid notifier program '{0}' for events '{1}".
+                                 format(fPath, eventTypes))
+            if not set(eventTypes).issubset(set(EVENT_TYPES)):
+                raise ValueError("Invalid event types '{0}' for notifier '{1}'".
+                                 format(set(eventTypes) - set(EVENT_TYPES), notifier))
+            for eventType in eventTypes:
+                if eventType not in self.notifiers:
+                    self.notifiers[eventType] = []
+                self.notifiers[eventType].append(fPath)
 
     def __str__(self):
-        return "Events/Notifiers: {0}".format(self.eventNotifiers)
+        s = "eventsNotifiers: "
+        j = json.dumps(self.notifiers, indent=4, sort_keys=True)
+        return s + j
 
-    def notify(self, eventType, arg):
-        #### FIXME
-        print("Event: {0}, Notifier: {1}({2})".format(eventType, self.eventNotifiers[eventType], arg))
+    def notify(self, eventType, arg=None):
+        if not arg:
+            raise ValueError("Must provide arg")
+        for notifier in self.notifiers[eventType]:
+            #### FIXME call all the notifiers (with the given arg) that ar associated with this event type
+            print("Event: {0}, Notifier: {1}({2})".format(eventType, notifier, arg))
+            if os.system("{0} {1}".format(notifier, arg)):
+                raise Exception("Command '{0}({1})' failed")
 
 
 #
 # TESTING
 #
 if __name__ == '__main__':
-    #### TODO implement test cases
-    pass
+    TEST_NOTIFIERS = {
+        'sms.py': [
+            "STOPPED_MOVING",
+            "STARTED_MOVING"
+        ],
+        'testScript.sh': [
+            "ENTER_REGION"
+        ]
+    }
+
+    # should work
+    try:
+        n = Notifier(TEST_NOTIFIERS)
+        print(n)
+
+        n.notify("STOPPED_MOVING", "xxx")
+
+        n.notify("ENTER_REGION", "yyy")
+    except Exception as e:
+        print("FAILED: {0}".format(e))
+        sys.exit(1)
+
+    # should fail
+    err = False
+    try:
+        n.notify("ENTER_REGION")
+    except Exception:
+        err = True
+    if not err:
+        print("FAILED: to fail")
+        sys.exit(1)
+
+    # should fail
+    err = False
+    try:
+        n = Notifier({'asdf': ["EXIT_REGION"]})
+        n.notify("EXIT_REGION")
+    except Exception:
+        err = True
+    if not err:
+        print("FAILED: to fail")
+        sys.exit(1)
+
+    # should fail
+    err = False
+    try:
+        n = Notifier({'sms.py': ["XYZZY"]})
+        n.notify("XYZZY")
+    except Exception:
+        err = True
+    if not err:
+        print("FAILED: to fail")
+        sys.exit(1)
+
+    print("SUCCEEDED")
